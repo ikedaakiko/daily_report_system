df42cd028e39775a7105d2b7068fa99f3bf5fa18
akiko.ikeda <ako_neco@yahoo.co.jp>
2020-06-26 22:35:07 +0900

    Add Employee edit

--- src/controllers/employees/EmployeesDestroyServlet.java
+++ src/controllers/employees/EmployeesDestroyServlet.java
@@ -34,18 +34,18 @@ public class EmployeesDestroyServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         // TODO Auto-generated method stub
         String _token = (String)request.getParameter("_token");
-        if(_token != null && _token.equals(request.getSession().getId())); {
+        if(_token != null && _token.equals(request.getSession().getId())) {
             EntityManager em = DBUtil.createEntityManager();
 
             Employee e = em.find(Employee.class, (Integer)(request.getSession().getAttribute("employee_id")));
-           e.setDelete_flag(1);
+            e.setDelete_flag(1);
             e.setUpdated_at(new Timestamp(System.currentTimeMillis()));
 
             em.getTransaction().begin();
             em.getTransaction().commit();
             em.close();
-
             request.getSession().setAttribute("flush", "削除が完了しました。");
+
             response.sendRedirect(request.getContextPath() + "employees/index");
         }
 
